9a103060f455b2fb49e63838d730269fbcc24971
nyny567567 <ny567@naver.com>
2023-08-25 14:05:46 +0900

    162쪽까지 완료

---
+++ guestbook/src/main/java/org/zerock/guestbook/service/GuestbookService.java
@@ -0,0 +1,22 @@
+package org.zerock.guestbook.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Service;
+import org.zerock.guestbook.dto.GuestbookDTO;
+import org.zerock.guestbook.entity.Guestbook;
+
+
+public interface GuestbookService {
+    Long register(GuestbookDTO dto);
+
+    default Guestbook dtoToEntity(GuestbookDTO dto) {
+        Guestbook entity = Guestbook.builder()
+                .gno(dto.getGno())
+                .title(dto.getTitle())
+                .content(dto.getContent())
+                .writer(dto.getWriter())
+                .build();
+        return entity;
+    }
+}
