c4fede07bf01457d5c78c661be80d2768d1732d2
nyny567567 <ny567@naver.com>
2023-08-27 15:54:20 +0900

    191쪽까지 완료

--- guestbook/src/main/java/org/zerock/guestbook/service/GuestbookServiceImpl.java
+++ guestbook/src/main/java/org/zerock/guestbook/service/GuestbookServiceImpl.java
@@ -12,6 +12,7 @@ import org.zerock.guestbook.dto.PageResultDTO;
 import org.zerock.guestbook.entity.Guestbook;
 import org.zerock.guestbook.repository.GuestbookRepository;
 
+import java.util.Optional;
 import java.util.function.Function;
 
 @Service
@@ -47,5 +48,13 @@ public class GuestbookServiceImpl implements GuestbookService {
 
         return new PageResultDTO<>(result, fn);
 
+    }
+
+    @Override
+    public GuestbookDTO read(Long gno) {
+
+        Optional<Guestbook> result = repository.findById(gno);
+
+        return result.isPresent()? entityToDTO(result.get()): null;
     }
 }
